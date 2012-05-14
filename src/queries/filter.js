83c34d9b20ad7d562da2b677e350b0e8d65d268e
Brian Noguchi <brian.noguchi@gmail.com>
2012-05-14 15:34:30 -0700

    Replaced Filter class with more functional createFilter

---
+++ src/queries/filter.js
@@ -0,0 +1,135 @@
+var lookup = require('../path').lookup
+  , transaction = require('../transaction')
+  , util = require('../util')
+  , indexOf = util.indexOf
+  , deepIndexOf = util.deepIndexOf
+  , deepEqual = util.deepEqual;
+
+module.exports = createFilter;
+
+// @param {Object} json representing a query that is typically created via
+// convenient QueryBuilder instances
+//
+// json looks like:
+// {
+//   from: 'collectionName'
+// , byKey: keyVal
+// , equals: {
+//     somePath: someVal
+// , }
+// , notEquals: {
+//     somePath: someVal
+//   }
+// , sort: ['fieldA', 'asc', 'fieldB', 'desc']
+// }
+function createFilter (json) {
+  // Stores a list of predicate functions that take a document and return a
+  // Boolean. If all predicate functions return true, then the document passes
+  // through the filter. If not, the document is blocked by the filter
+  var predicates = []
+    , pred;
+
+  if (json) for (var method in json) {
+    pred = predicateBuilders[method](json[method]);
+    if (Array.isArray(pred)) predicates = predicates.concat(pred);
+    else predicates.push(pred);
+  }
+
+  return compileDocFilter(predicates);
+}
+
+var predicateBuilders = {};
+
+predicateBuilders.from = function from (ns) {
+  return function (doc, docNs) { return ns === docNs; };
+};
+
+predicateBuilders.byKey = function byKey (keyVal) {
+  return function (doc) { return doc.id === keyVal; };
+};
+
+var fieldPredicates = {
+    equals: function (fieldName, val, doc) {
+      var currVal = lookup(fieldName, doc);
+      if (typeof currVal === 'object') {
+        return deepEqual(currVal, val);
+      }
+      return currVal === val;
+    }
+  , notEquals: function (fieldName, val, doc) {
+      var currVal = lookup(fieldName, doc);
+      if (typeof currVal === 'object') {
+        return ! deepEqual(currVal, val);
+      }
+      return currVal !== val;
+    }
+  , gt: function (fieldName, val, doc) {
+      return lookup(fieldName, doc) > val;
+    }
+  , gte: function (fieldName, val, doc) {
+      return lookup(fieldName, doc) >= val;
+    }
+  , lt: function (fieldName, val, doc) {
+      return lookup(fieldName, doc) < val;
+    }
+  , lte: function (fieldName, val, doc) {
+      return lookup(fieldName, doc) <= val;
+    }
+  , within: function (fieldName, list, doc) {
+      if (!list.length) return false;
+      var x = lookup(fieldName, doc);
+      if (x.constructor === Object) return ~deepIndexOf(list, x);
+      return ~list.indexOf(x);
+    }
+  , contains: function (fieldName, list, doc) {
+      var docList = lookup(fieldName, doc);
+      if (typeof docList === 'undefined') {
+        if (list.length) return false;
+        return true; // contains nothing
+      }
+      for (var x, i = list.length; i--; ) {
+        x = list[i];
+        if (x.constructor === Object) {
+          if (-1 === deepIndexOf(docList, x)) return false;
+        } else {
+          if (-1 === docList.indexOf(x)) return false;
+        }
+      }
+      return true;
+    }
+};
+
+for (var queryKey in fieldPredicates) {
+  predicateBuilders[queryKey] = (function (fieldPred) {
+    return function (params) {
+      return createDocPredicates(params, fieldPred);
+    };
+  })(fieldPredicates[queryKey]);
+}
+
+function createDocPredicates (params, fieldPredicate) {
+  var predicates = []
+    , docPred;
+  for (var fieldName in params) {
+    docPred = fieldPredicate.bind(undefined, fieldName, params[fieldName]);
+    predicates.push(docPred);
+  }
+  return predicates;
+};
+
+
+function compileDocFilter (predicates) {
+  switch (predicates.length) {
+    case 0: return evalToTrue;
+    case 1: return predicates[0];
+  }
+  return function test (doc, ns) {
+    if (typeof doc === 'undefined') return false;
+    for (var i = 0, l = predicates.length; i < l; i++) {
+      if (! predicates[i](doc, ns)) return false;
+    }
+    return true;
+  };
+}
+
+function evalToTrue () { return true; }
