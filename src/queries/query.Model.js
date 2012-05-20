268855a7a7713f10e0f184220e60759fa46694bc
Brian Noguchi <brian.noguchi@gmail.com>
2012-05-19 20:45:45 -0700

    Add query.Model and query.Store mixin

---
+++ src/queries/query.Model.js
@@ -0,0 +1,132 @@
+var ModelQueryBuilder = require('./ModelQueryBuilder')
+  , QueryBuilder = require('./QueryBuilder')
+  , path = require('../path')
+  , splitPath = path.split
+  , expandPath = path.expand
+  ;
+
+module.exports = {
+  type: 'Model'
+, events: {
+    init: function (model) {
+      // maps hash -> 1
+      model._privateQueries = {}
+
+      // maps hash ->
+      //        query: QueryBuilder
+      //        memoryQuery: MemoryQuery
+      model._queries = {}
+    }
+  }
+, proto: {
+
+    query: function (namespace, queryParams) {
+      queryParams || (queryParams = {});
+      queryParams.from = namespace;
+      return new ModelQueryBuilder(queryParams, this);
+    }
+
+  , findOne: function (namespace, queryParams) {
+      queryParams || (queryParams = {});
+      queryParams.from = namespace;
+      return (new ModelQueryBuilder(queryParams, this)).findOne();
+    }
+
+  , find: function (namespace, queryParams) {
+      queryParams || (queryParams = {});
+      queryParams.from = namespace;
+      return (new ModelQueryBuilder(queryParams, this)).find();
+    }
+
+    // fetch(targets..., callback)
+  , fetch: function () {
+      this._compileTargets(arguments, {
+        compileModelAliases: true
+      , eachQueryTarget: function (queryJson, addToTargets) {
+          addToTargets(queryJson);
+        }
+      , eachPathTarget: function (path, addToTargets) {
+          addToTargets(path);
+        }
+      , done: function (targets, modelAliases, fetchCb) {
+          var self = this;
+          this._fetch(targets, function (err, data) {
+              self._addData(data);
+              fetchCb.apply(null, [err].concat(modelAliases));
+          });
+        }
+      });
+    }
+
+  , _fetch: function (targets, cb) {
+      if (!this.connected) return cb('disconnected');
+      this.socket.emit('fetch', targets, cb);
+    }
+
+    // _arguments is an Array-like arguments whose members are either
+    // QueryBuilder instances
+  , _compileTargets: function (_arguments, opts) {
+      var arglen = _arguments.length
+        , last = _arguments[arglen-1]
+        , hasCb = (typeof last === 'function')
+        , cb = hasCb ? last : noop
+        , i = hasCb ? arglen-1 : arglen
+
+        , newTargets = []
+
+        , eachQueryTarget = opts.eachQueryTarget
+        , eachPathTarget = opts.eachPathTarget
+        , done = opts.done
+
+        , self = this;
+
+      if (opts.compileModelAliases) {
+        var modelAliases = []
+          , aliasPath;
+      }
+
+      function addToTargets (target) {
+        newTargets.push(target);
+      }
+
+      // Transform incoming targets into full set of `newTargets`.
+      // Compile the list `out` of model aliases representative of the fetched
+      // results, to pass back to the callback `cb`
+      while (i--) {
+        var target = _arguments[i];
+        if (target instanceof QueryBuilder || target instanceof ModelQueryBuilder) {
+          var queryJson = target.toJSON();
+          if (modelAliases) aliasPath = '_$queries.' + QueryBuilder.hash(queryJson) + '._results';
+          eachQueryTarget.call(this, queryJson, addToTargets, aliasPath);
+          newTargets.push(queryJson);
+        } else { // Otherwise, target is a path or model alias
+          if (target._at) target = target._at;
+          if (modelAliases) aliasPath = splitPath(target)[0];
+          var paths = expandPath(target);
+          for (var k = paths.length; k--; ) {
+            var path = paths[k];
+            eachPathTarget.call(this, path, addToTargets, aliasPath);
+          }
+        }
+        if (modelAliases) {
+          modelAliases.push(this.at(aliasPath, true));
+        }
+      }
+
+      if (modelAliases) {
+        done.call(this, newTargets, modelAliases, cb);
+      } else {
+        done.call(this, newTargets, cb);
+      }
+    }
+  }
+, server: {
+    _fetch: function (targets, cb) {
+      var store = this.store;
+      this._clientIdPromise.on( function (err, clientId) {
+        if (err) return cb(err);
+        store.fetch(clientId, targets, cb);
+      });
+    }
+  }
+};
---
+++ src/queries/query.Store.js
@@ -0,0 +1,150 @@
+var QueryHub = require('./QueryHub')
+  , path = require('../path')
+  , splitPath = path.split
+  , lookup = path.lookup
+  , finishAfter = require('../util/async').finishAfter
+  ;
+
+module.exports = {
+  type: 'Store'
+
+, events: {
+    init: function (store, opts) {
+      store._queryCoordinator = new QueryHub(store);
+    }
+  , socket: function (store, socket, clientId) {
+      socket.on('fetch', function (targets, cb) {
+        // Only fetch data
+        store.fetch(clientId, targets, cb);
+      });
+
+      socket.on('fetchCurrSnapshot', function (ver, clientStartId, subs) {
+        store._onSnapshotRequest(ver, clientStartId, clientId, socket, subs);
+      });
+    }
+  }
+
+, proto: {
+
+    // @param {QueryBuilder} query
+    // @param {Function} cb
+    query: function (queryJson, cb) {
+      return this._queryCoordinator.fetch(queryJson, cb);
+    }
+  , fetch: function (clientId, targets, cb) {
+      var data = []
+        , self = this
+        , finish = finishAfter(targets.length, function (err) {
+            if (err) return cb(err);
+            var out = {data: data};
+            // Note that `out` may be mutated by ot or other plugins
+            self.emit('fetch', out, clientId, targets);
+            cb(null, out);
+          });
+      for (var i = 0, l = targets.length; i < l; i++) {
+        var target = targets[i];
+        var fetchFn = ('string' === typeof target)
+                    ? this._fetchPathData
+                    : this._fetchQueryData;
+        fetchFn.call(this, target, function (path, datum, ver) {
+          data.push([path, datum, ver]);
+        }, finish);
+      }
+    }
+    // TODO Add in an optimization later since query._paginatedCache
+    // can be read instead of going to the db. However, we must make
+    // sure that the cache is a consistent snapshot of a given moment
+    // in time. i.e., no versions of the cache should exist between
+    // an add/remove combined action that should be atomic but currently
+    // isn't
+    // TODO Get version consistency right in face of concurrent writes
+    // during query
+  , _fetchQueryData: function (queryJson, eachDatumCb, finish) {
+      this.query(queryJson, function (err, result, version) {
+        if (err) return finish(err);
+        var path;
+        if (Array.isArray(result)) {
+          for (var i = result.length; i--; ) {
+            var doc = result[i];
+            path = queryJson.from + '.' + doc.id;
+            eachDatumCb(path, doc, version);
+          }
+        } else if (result) {
+          path = queryJson.from + '.' + result.id;
+          eachDatumCb(path, result, version);
+        }
+        finish(null);
+      });
+    }
+  , _fetchPathData: function (path, eachDatumCb, finish) {
+      var parts = splitPath(path)
+        , root = parts[0]
+        , remainder = parts[1];
+      this.get(root, function (err, datum, ver) {
+        if (err) return finish(err);
+        if (typeof remainder === 'undefined') {
+          eachDatumCb(path, datum, ver);
+        } else {
+          // The path looks like <root>.*.<remainder>
+          // so set each property one level down
+          patternMatchingDatum(root, remainder, datum, function (fullPath, datum) {
+            eachDatumCb(fullPath, datum, ver);
+          });
+        }
+        return finish(null);
+      });
+    }
+  , _onSnapshotRequest: function (ver, clientStartId, clientId, socket, subs, shouldSubscribe) {
+      var self = this;
+      this._checkVersion(ver, clientStartId, function (err) {
+        if (err) return socket.emit('fatalErr', err);
+        if (shouldSubscribe) {
+          self._pubSub.subscribe(clientId, subs);
+        }
+        self._mode.snapshotSince({
+            ver: ver
+          , clientId: clientId
+          , subs: subs
+        }, function (err, payload) {
+          if (err) return socket.emit('fatalErr', err);
+          var data = payload.data
+            , txns = payload.txns
+            , num  = self._txnClock.nextTxnNum(clientId);
+          if (data) {
+            socket.emit('snapshotUpdate:replace', data, num);
+          } else if (txns) {
+            var len;
+            if (len = txns.length) {
+              socket.__Ver = transaction.getVer(txns[len-1]);
+            }
+            socket.emit('snapshotUpdate:newTxns', txns, num);
+          }
+        });
+      });
+    }
+  }
+};
+
+// @param {String} prefix is the part of the path up to ".*."
+// @param {String} remainder is the part of the path after ".*."
+// @param {Object} subDoc is the lookup value of the prefix
+// @param {Function} eachDatumCb is the callback for each datum matching the pattern
+function patternMatchingDatum (prefix, remainder, subDoc, eachDatumCb) {
+  var parts          = splitPath(remainder)
+    , appendToPrefix = parts[0]
+    , remainder      = parts[1];
+  for (var property in subDoc) {
+    var value = subDoc[property];
+    if (value.constructor !== Object && ! Array.isArray(value)) {
+      // We can't lookup `appendToPrefix` on `value` in this case
+      continue;
+    }
+    var newPrefix = prefix + '.' + property + '.' + appendToPrefix
+      , newValue = lookup(appendToPrefix, value);
+    if (typeof remainder === 'undefined') {
+      eachDatumCb(newPrefix, newValue);
+    } else {
+      patternMatchingDatum(newPrefix, remainder, newValue, eachDatumCb);
+    }
+  }
+}
