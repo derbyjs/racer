8613339d61bed97cb1763a410cb24a4e0657ae85
Brian Noguchi <brian.noguchi@gmail.com>
2012-05-25 19:51:38 -0700

    Replace racer.coffee -> racer.js

---
+++ src/racer.js
@@ -0,0 +1,45 @@
+var util = require('./util')
+  , mergeAll = util.mergeAll
+  , isServer = util.isServer
+  , isClient = !isServer;
+
+if (isClient) require('es5-shim');
+
+var EventEmitter = require('events').EventEmitter
+  , plugin = require('./plugin');
+
+var racer = module.exports = new EventEmitter();
+
+mergeAll(racer, plugin, {
+  version: require('../package.json').version
+, isServer: isServer
+, isClient: isClient
+, protected: {
+    Model: require('./Model')
+  }
+, util: util
+});
+
+// Note that this plugin is passed by string to prevent Browserify from
+// including it
+if (isServer) {
+  racer.use(__dirname + '/racer.server');
+}
+
+racer
+  .use(require('./mutators'))
+  .use(require('./refs'))
+  .use(require('./pubSub'))
+  .use(require('./queries'))
+  .use(require('./txns'))
+  .use(require('./reconnect'));
+
+if (isServer) {
+  racer.use(__dirname + '/adapters/pubsub-memory');
+}
+
+// The browser module must be included last, since it creates a model instance,
+// before which all plugins should be included
+if (isClient) {
+  racer.use(require('./racer.browser'));
+}
