0c6e1f7ca7f02e9e4e72a167ca0da4d9fd9b008c
Brian Noguchi <brian.noguchi@gmail.com>
2012-05-29 18:25:02 -0700

    coffee -> js for refs/refList

---
+++ src/refs/refList.js
@@ -0,0 +1,184 @@
+var hasKeys = require('../util').hasKeys
+  , refUtils = require('./util')
+  , derefPath = refUtils.derefPath
+  , lookupPath = refUtils.lookupPath
+  , Model = require('../Model')
+  , addListener = require('./ref').addListener
+  ;
+
+module.exports = createRefList;
+
+function createRefList (model, from, to, key) {
+  if (!from || !to || !key) {
+    throw new Error('Invalid arguments for model.refList');
+  }
+  var listeners = []
+    , arrayMutators = Model.arrayMutator
+
+    , getter = createGetter(from, to, key);
+
+  addListener(model, from, getter, listeners, key, function (match, method, args) {
+    var i = arrayMutators[method] && arrayMutators[method].insertArgs;
+    if (i) {
+      var id;
+      while ((id = args[i]) && id != null) {
+        args[i] = model.get(to + '.' + id);
+        i++;
+      }
+    }
+    return from;
+  });
+
+  addListener(model, from, getter, listeners, to + '.*', function (match) {
+    var id = match[1]
+      , i = id.indexOf('.')
+      , remainder;
+    if (~i) {
+      remainder = id.substr(i+1);
+      id = id.substr(0, i);
+    }
+    var pointerList = model.get(key);
+    if (!pointerList) return null;
+    i = pointerList.indexOf(id);
+    if (i === -1) return null;
+    return remainder ?
+      from + '.' + i + '.' + remainder :
+      from + '.' + i;
+  });
+
+  return getter;
+}
+
+function createGetter (from, to, key) {
+  /**
+   * This represents a ref function that is assigned as the value of the node
+   * located at `path` in `data`
+   *
+   * @param {Function} lookup is the Memory lookup function
+   * @param {Object} data is the speculative or non-speculative data tree
+   * @param {String} path is the current path to the ref function
+   * @param {[String]} props is the chain of properties representing a full
+   * path, of which path may be just a sub path
+   * @param {Number} i is the array index of props that we are currently at
+   * @return {Array} [evaled, path, i] where
+   */
+  return function getter (lookup, data, path, props, len, i) {
+    var basicMutators = Model.basicMutator
+      , arrayMutators = Model.arrayMutator
+
+      , obj = lookup(to, data) || {}
+      , dereffed = derefPath(data, to);
+    data.$deref = null;
+    var pointerList = lookup(key, data)
+      , dereffedKey = derefPath(data, key)
+      , currPath, id;
+
+    if (i === len) {
+      // Method is on the refList itself
+      currPath = lookupPath(dereffed, props, i);
+
+      data.$deref = function (method, args, model) {
+        if (method in basicMutators) return path;
+
+        var mutator, j, arg, indexArgs;
+        if (mutator = arrayMutators[method]) {
+          // Handle index args if they are specified by id
+          if (indexArgs = mutator.indexArgs) for (var k = 0, kk = indexArgs.length; k < kk; k++) {
+            j = indexArgs[k]
+            arg = args[j];
+            if (!arg) continue;
+            id = arg.id;
+            if (id == null) continue;
+            // Replace id arg with the current index for the given id
+            var idIndex = pointerList.indexOf(id);
+            if (idIndex !== -1) args[j] = idIndex;
+          } // end if (indexArgs)
+
+          if (j = mutator.insertArgs) while (arg = args[j]) {
+            id = (arg.id != null)
+               ? arg.id
+               : (arg.id = model.id());
+            // Set the object being inserted if it contains any properties
+            // other than id
+            if (hasKeys(arg, 'id')) {
+              model.set(dereffed + '.' + id, arg);
+            }
+            args[j] = id;
+            j++;
+          }
+
+          return dereffedKey;
+        }
+
+        throw new Error(method + ' unsupported on refList');
+      }; // end of data.$deref function
+
+      if (pointerList) {
+        var curr = [];
+        for (var k = 0, kk = pointerList.length; k < kk; k++) {
+          var prop = pointerList[k];
+          curr.push(obj[prop]);
+        }
+        return [curr, currPath, i];
+      }
+
+      return [undefined, currPath, i];
+
+    } else { // if (i !== len)
+      var index = props[i++]
+        , prop, curr;
+
+      if (pointerList && (prop = pointerList[index])) {
+        curr = obj[prop];
+      }
+
+      if (i === len) {
+        // Method is on an index of refList
+        currPath = lookupPath(dereffed, props, i);
+
+        data.$deref = function (method, args, model, obj) {
+          // TODO Additional model methods should be done atomically with the
+          // original txn instead of making an additional txn
+
+          var value, id;
+          if (method === 'set') {
+            value = args[1];
+            id = (value.id != null)
+               ? value.id
+               : (value.id = model.id());
+            if (pointerList) {
+              model.set(dereffedKey + '.' + index, id);
+            } else {
+              model.set(dereffedKey, [id]);
+            }
+            return currPath + '.' + id;
+          }
+
+          if (method === 'del') {
+            id = obj.id;
+            if (id == null) {
+              throw new Error('Cannot delete refList item without id');
+            }
+            model.del(dereffedKey + '.' + index);
+            return currPath + '.' + id;
+          }
+
+          throw new Error(method + ' unsupported on refList index');
+        } // end of data.$deref function
+
+      } else { // if (i !== len)
+        // Method is on a child of the refList
+        currPath = lookupPath(dereffed + '.' + prop, props, i);
+
+        data.$deref = function (method) {
+          if (method && prop == null) {
+            throw new Error(method + ' on undefined refList child ' + props.join('.'));
+          }
+          return currPath;
+        };
+      }
+
+      return [curr, currPath, i];
+    }
+  };
+}
