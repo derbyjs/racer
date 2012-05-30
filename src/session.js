07dd6245413f169227531d9c6b02072cabb99547
Brian Noguchi <brian.noguchi@gmail.com>
2012-05-29 23:23:14 -0700

    coffee -> js session

---
+++ src/session.js
@@ -0,0 +1,34 @@
+// TODO Implement proper session security
+
+module.exports = function (store) {
+  // The actual middleware is created by a factory so that the store can be set
+  // later
+  var fn = function (req, res, next) {
+    if (!req.session) throw new Error('Missing session middleware');
+    fn = sessionFactory(store);
+    fn(req, res, next);
+  };
+
+  function middleware (req, res, next) {
+    return fn(req, res, next);
+  }
+
+  middleware._setStore = function (_store) {
+    store = _store;
+  };
+
+  return middleware;
+};
+
+function sessionFactory (store) {
+  return function (req, res, next) {
+    // Make sure to use only the unsalted id in data exposed to the client
+    var sid = req.sessionID;
+
+    var model = req.model || (req.model = store.createModel());
+    model.subscribe( 'sessions.' + sid, function (err, session) {
+      model.ref('_session', session);
+      next();
+    });
+  };
+}
