ffa5d6e97a03538130d6e26a33aa0c5dce5c09a9
Brian Noguchi <brian.noguchi@gmail.com>
2012-06-01 15:34:57 -0700

    coffee -> js for racer.browser and racer.server

---
+++ src/racer.browser.js
@@ -0,0 +1,59 @@
+/** WARNING
+ * All racer modules for the browser should be included in racer.coffee and not
+ * in this file.
+ */
+
+// Static isReady and model variables are used, so that the ready function can
+// be called anonymously. This assumes that only one instance of Racer is
+// running, which should be the case in the browser.
+var IS_READY,
+    model;
+
+IS_READY = model = null;
+
+exports = module.exports = plugin;
+exports.useWith = { server: false, browser: true };
+exports.decorate = 'racer';
+
+function plugin (racer) {
+  racer.init = function (tuple, socket) {
+    var clientId = tuple[0]
+      , memory   = tuple[1]
+      , count    = tuple[2]
+      , onLoad   = tuple[3]
+      , startId  = tuple[4]
+      , ioUri    = tuple[5];
+
+    model = new this.protected.Model;
+    model._clientId = clientId;
+    model._startId  = startId;
+    model._memory.init(memory);
+    model._count = count;
+
+    for (var i = 0, l = onLoad.length; i < l; i++) {
+      var item = onLoad[i]
+        , method = item.shift();
+      model[method].apply(model, item);
+    }
+
+    racer.emit('init', model);
+
+    // TODO If socket is passed into racer, make sure to add clientId query param
+    model._setSocket(socket || io.connect(ioUri), {
+      'reconnection delay': 100
+    , 'max reconnection attempts': 20
+    , query: 'clientId=' + clientId
+    });
+
+    IS_READY = true;
+    racer.emit('ready', model);
+    return racer;
+  };
+
+  racer.ready = function (onready) {
+    return function () {
+      if (IS_READY) return onready(model);
+      racer.on('ready', onready);
+    };
+  }
+}
---
+++ src/racer.server.js
@@ -0,0 +1,123 @@
+var fs = require('fs')
+  , browserify = require('browserify')
+  , socketio = require('socket.io')
+  , socketioClient = require('socket.io-client')
+  , uglify = require('uglify-js')
+  , Store = require('./Store')
+  , util = require('./util')
+  , mergeAll = util.mergeAll
+  , isProduction = util.isProduction;
+
+exports = module.exports = plugin;
+exports.decorate = 'racer';
+exports.useWith = { server: true, browser: false };
+
+function plugin (racer) {
+  racer.version = require('../package.json').version;
+
+  /* For use by all other plugins */
+  mergeAll(racer.protected, {
+    // TODO This is brittle if we change what is in ./queries/ later. Instead,
+    // we should add a "protected" attribute to particular objects in these
+    // directories, and then compile these objects into racer.protected
+    queries: {
+      QueryBuilder: require('./queries/QueryBuilder')
+    , ModelQueryBuilder: require('./queries/ModelQueryBuilder')
+    , MemoryQuery: require('./queries/MemoryQuery')
+    , QueryHub: require('./queries/QueryHub')
+    , QueryNode: require('./queries/QueryNode')
+    , PaginatedQueryNode: require('./queries/PaginatedQueryNode')
+    , filter: require('./queries/filter')
+    }
+  , diffMatchPatch: require('./diffMatchPatch')
+  , Memory: require('./Memory')
+  , path: require('./path')
+  , Serializer: require('./Serializer')
+  , Store: Store
+  , transaction: require('./transaction.server')
+  });
+
+  /* Racer Server-side Configuration */
+
+  racer.settings = { env: process.env.NODE_ENV || 'development' };
+
+  racer.configure = function () {
+    var envs = Array.prototype.slice.call(arguments, 0, arguments.length-1)
+      , fn = arguments[arguments.length-1];
+    if (envs[0] === 'all' || ~envs.indexOf(this.settings.env)) {
+      fn.call(this);
+    }
+    return this;
+  };
+
+  racer.set = function (setting, value) {
+    this.settings[setting] = value;
+    return this;
+  };
+
+  racer.get = function (setting) { return this.settings[setting]; };
+
+  racer.set('transports', ['websocket', 'xhr-polling']);
+
+  racer.configure('production', function () {
+    this.set('minify', true);
+  });
+
+  /* Racer built-in features */
+
+  racer.createStore = function (options) {
+    options || (options = {});
+    // TODO Provide full configuration for socket.io
+    var store = new Store(options)
+      , sockets, listen;
+    if (sockets = options.sockets) {
+      store.setSockets(sockets, options.socketUri);
+    } else if (listen = options.listen) {
+      store.listen(listen, options.namespace);
+    }
+    return store;
+  };
+
+  /**
+   * Returns a string of JavaScript representing a browserify bundle and the
+   * socket.io client-side code.
+   *
+   * Options:
+   *   minify: Set to truthy to minify the JavaScript
+   *
+   *   Passed to browserify:
+   *     entry: e.g., __dirname + '/client.js'
+   *     filter: defaults to uglify if minify is true
+   *     debug: true unless in production
+   */
+  racer.js = function (options, callback) {
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+    var minify = options.minify || this.get('minify');
+    if (minify && !options.filter) {
+      options.filter = this.get('minifyFilter') || uglify;
+    }
+
+    // Add pseudo filenames and line numbers in browser debugging
+    if (! isProduction && options.debug != null) {
+      options.debug = true;
+    }
+
+    socketioClient.builder(this.get('transports'), {minify: minify}, function (err, value) {
+      callback(err, value + ';' + browserify.bundle(options));
+    });
+  };
+
+  racer.session = require('./session');
+  racer.registerAdapter = require('./adapters').registerAdapter;
+
+  racer
+    .use(require('./bundle.Model'))
+    .use(require('./adapters/db-memory'))
+    .use(require('./adapters/journal-memory'))
+    .use(require('./adapters/clientid-mongo'))
+    .use(require('./adapters/clientid-redis'))
+    .use(require('./adapters/clientid-rfc4122_v4'))
+}
