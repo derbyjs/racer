6d82914daf0aaace5553a47660947f6587447eed
Brian Noguchi <brian.noguchi@gmail.com>
2012-06-25 18:27:49 -0700

    More robust fix for QueryNode publish decision edge case of new doc creation

--- src/adapters/pubsub-memory/channel-interface-query.js
+++ src/adapters/pubsub-memory/channel-interface-query.js
@@ -25,25 +25,7 @@ function createQueryInterface (pubSub, store) {
           , txn = params.data
           , origDoc = meta.origDoc
           , newDoc;
-        if (origDoc) {
-          newDoc = deepCopy(origDoc);
-        } else {
-          // Otherwise this is a new doc
-          var setVal = transaction.getArgs(txn)[1]
-            , path = transaction.getPath(txn)
-            , parts = path.split('.');
-          if (parts.length === 2) {
-            newDoc = setVal;
-          } else {
-            newDoc = { id: parts[1] };
-            var curr = newDoc;
-            for (var i = 2, l = parts.length; i < l - 1; i++) {
-              var prop = parts[i];
-              curr = curr[prop] = {};
-            }
-            curr[parts[l-1]] = setVal;
-          }
-        }
+        if (origDoc) newDoc = deepCopy(origDoc);
         newDoc = applyTxn(txn, newDoc);
         queryCoordinator.publish(newDoc, origDoc, txn);
       }
@@ -62,7 +44,7 @@ function applyTxn (txn, doc) {
     , world = {}
     , data = { world: world };
   world[ns] = {};
-  world[ns][id] = doc;
+  if (doc) world[ns][id] = doc;
 
   transaction.applyTxn(txn, data, memory, -1);
   return memory.get(ns + '.' + id, data);
