ead87af073b3fb199ca9e4463f42d0dd29045fe2
rma4ok <rma4ok@gmail.com>
2012-07-11 14:56:00 -0400

    [fix] emission on set and del of parents: check for object before lookup

--- src/refs/ref.js
+++ src/refs/ref.js
@@ -165,12 +165,13 @@ function setupRefWithoutKeyListeners(model, from, to, getter) {
   addListener(listeners, model, from, getter, parents, function (match, mutator, args) {
     var path = match.input
       , remainder = to.slice(path.length + 1)
+      , argsOut = args.out, args1 = args[1]
 
     if (mutator === 'set') {
-      args[1] = lookup(remainder, args[1]);
-      args.out = lookup(remainder, args.out);
+      args[1] = args1 && lookup(remainder, args1);
+      args.out = argsOut && lookup(remainder, argsOut);
     } else if (mutator === 'del') {
-      args.out = lookup(remainder, args.out);
+      args.out = argsOut && lookup(remainder, argsOut);
     } else {
       // Don't emit an event if not a set or delete
       return null;
