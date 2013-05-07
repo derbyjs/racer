89dbe119e676a885d0e56485af0bd19e0a519078
Nate Smith <nate@nateps.com>
2013-05-06 19:45:25 -0700

    cleanup pad example a bit

--- examples/hello/server.js
+++ examples/hello/server.js
@@ -1,49 +1,50 @@
-var http = require('http');
 var fs = require('fs');
+var http = require('http');
 var express = require('express');
 var handlebars = require('handlebars');
 var racer = require('../../lib/racer');
 var share = require('share');
 
-var app = express()
-  .use(express.favicon())
-
+var app = express();
 var server = http.createServer(app);
 var store = racer.createStore({
   server: server
 , db: share.db.mongo('localhost:27017/test?auto_reconnect', {safe: true})
 });
 
-app.use(store.socketMiddleware());
+app
+  .use(express.favicon())
+  .use(store.socketMiddleware())
+  .use(store.modelMiddleware())
 
-app.get('/script.js', function(req, res) {
+app.get('/script.js', function(req, res, next) {
   racer.bundle(__dirname + '/client.js', function(err, js) {
+    if (err) return next(err);
     res.type('js');
     res.send(js);
   });
 });
 
-app.get('/:roomId', function(req, res, next) {
-  var model = store.createModel();
-  var index = fs.readFileSync(__dirname + '/index.handlebars', 'utf-8');
-  var indexTemplate = handlebars.compile(index);
+var indexTemplate = fs.readFileSync(__dirname + '/index.handlebars', 'utf-8');
+var indexPage = handlebars.compile(indexTemplate);
 
-  var roomId = req.params.roomId;
-  var roomQuery = model.query('rooms', {_id:roomId});
-  roomQuery.subscribe(function(err) {
+app.get('/:roomId', function(req, res, next) {
+  var model = req.getModel();
+
+  var roomPath = 'rooms.' + req.params.roomId;
+  model.subscribe(roomPath, function(err) {
     if (err) return next(err);
 
-    model.ref('_room', 'rooms.' + roomId);
-
+    model.ref('_room', roomPath);
     model.bundle(function(err, bundle) {
       if (err) return next(err);
-      var html = indexTemplate({
+      var html = indexPage({
         text: model.get('_room')
       , bundle: bundle.replace(/<\//g, '<\\/')
       });
       res.send(html);
     });
-  })
+  });
 });
 
 app.get('/', function(req, res) {
@@ -52,5 +53,5 @@ app.get('/', function(req, res) {
 
 var port = process.env.PORT || 3000;
 server.listen(port, function() {
-  console.log("Go to http://localhost:" + port);
+  console.log('Go to http://localhost:' + port);
 });
