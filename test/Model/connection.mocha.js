904aaa751c0cf41d7c78ff580b04cfac706c2980
Nate Smith <nate@nateps.com>
2016-03-23 14:14:42 -0700

    add Model::getAgent()

---
+++ test/Model/connection.mocha.js
@@ -0,0 +1,26 @@
+var expect = require('../util').expect;
+var racer = require('../../lib/index');
+
+describe('connection', function() {
+
+  describe('getAgent', function() {
+
+    it('returns a reference to the ShareDB agent on the server', function() {
+      var backend = racer.createBackend();
+      var model = backend.createModel();
+      var agent = model.getAgent();
+      expect(agent).ok();
+    });
+
+    it('returns null once the model is disconnected', function(done) {
+      var backend = racer.createBackend();
+      var model = backend.createModel();
+      model.close(function() {
+        var agent = model.getAgent();
+        expect(agent).equal(null);
+        done();
+      });
+    });
+
+  });
+});
