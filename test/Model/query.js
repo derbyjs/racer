9d6b56628ed21aebf7fff94cb7c0def0469c5991
Eric Hwang <e@lever.co>
2017-07-28 13:16:58 -0700

    Change Query.idMap to count ids, to be defensive against duplicate ids in query results (see https://github.com/share/sharedb-mongo/issues/55)

--- test/Model/query.js
+++ test/Model/query.js
@@ -1,4 +1,5 @@
 var expect = require('../util').expect;
+var racer = require('../../lib');
 var Model = require('../../lib/Model');
 
 describe('query', function() {
@@ -12,6 +13,31 @@ describe('query', function() {
       var model = new Model();
       var query = model.query('foo', [{x: undefined}, {x: {y: undefined, z: 0}}]);
       expect(query.expression).eql([{x: null}, {x: {y: null, z: 0}}]);
+    });
+  });
+
+  describe('idMap', function() {
+    beforeEach('create in-memory backend and model', function() {
+      this.backend = racer.createBackend();
+      this.model = this.backend.createModel();
+    });
+    it('handles insert and remove of a duplicate id', function() {
+      var query = this.model.query('myCollection', {key: 'myVal'});
+      query.subscribe();
+      query.shareQuery.emit('insert', [
+        {id: 'a'},
+        {id: 'b'},
+        {id: 'c'},
+      ], 0);
+      // Add and immediately remove a duplicate id.
+      query.shareQuery.emit('insert', [
+        {id: 'a'},
+      ], 3);
+      query.shareQuery.emit('remove', [
+        {id: 'a'},
+      ], 3);
+      // 'a' is still present once in the results, should still be in the map.
+      expect(query.idMap).to.only.have.keys(['a', 'b', 'c']);
     });
   });
 });
