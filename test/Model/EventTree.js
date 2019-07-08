44562986a211fd8f9893cf3e574f59c4e6d3f4c5
Nate Smith <nate@lever.co>
2019-07-08 11:59:19 -0700

    initial implementation of EventTree
    A tree style of event emitter designed for internal use in making model.start more efficient. Could also be used to re-implement model events more efficiently and avoid regular expressions

---
+++ test/Model/EventTree.js
@@ -0,0 +1,179 @@
+var expect = require('../util').expect;
+var EventTree = require('../../lib/Model/EventTree');
+
+describe('EventTree', function() {
+  describe('implementation', function() {
+    describe('constructor', function() {
+      it('creates an empty tree', function() {
+        var tree = new EventTree();
+        expect(tree.listeners).equal(null);
+        expect(tree.children).equal(null);
+      });
+    });
+    describe('addListener', function() {
+      it('adds a listener object at the root', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener([], listener);
+        expect(tree.listeners).eql([listener]);
+        expect(tree.children).eql(null);
+      });
+      it('only a listener object once', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener([], listener);
+        tree.addListener([], listener);
+        expect(tree.listeners).eql([listener]);
+        expect(tree.children).eql(null);
+      });
+      it('adds a listener object at a path', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener(['colors'], listener);
+        expect(tree.listeners).eql(null);
+        expect(tree.children.colors.listeners).eql([listener]);
+      });
+      it('adds a listener object at a subpath', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener(['colors', 'green'], listener);
+        expect(tree.listeners).eql(null);
+        expect(tree.children.colors.listeners).eql(null);
+        expect(tree.children.colors.children.green.listeners).eql([listener]);
+      });
+    });
+    describe('removeListener', function() {
+      it('can be called before addListener', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.removeListener([], listener);
+        expect(tree.listeners).eql(null);
+        expect(tree.children).eql(null);
+      });
+      it('removes listener at root', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener([], listener);
+        expect(tree.listeners).eql([listener]);
+        tree.removeListener([], listener);
+        expect(tree.listeners).eql(null);
+      });
+      it('removes listener at subpath', function() {
+        var tree = new EventTree();
+        var listener = {};
+        tree.addListener(['colors', 'green'], listener);
+        expect(tree.children.colors.children.green.listeners).eql([listener]);
+        tree.removeListener(['colors', 'green'], listener);
+        expect(tree.children).eql(null);
+      });
+      it('removes listener with remaining peers', function() {
+        var tree = new EventTree();
+        var listener1 = 'listener1';
+        var listener2 = 'listener2';
+        var listener3 = 'listener3';
+        tree.addListener([], listener1);
+        tree.addListener([], listener2);
+        tree.addListener([], listener3);
+        expect(tree.listeners).eql([listener1, listener2, listener3]);
+        tree.removeListener([], listener2);
+        expect(tree.listeners).eql([listener1, listener3]);
+        tree.removeListener([], listener3);
+        expect(tree.listeners).eql([listener1]);
+        tree.removeListener([], listener1);
+        expect(tree.listeners).eql(null);
+      });
+      it('removes listener with remaining peer children', function() {
+        var tree = new EventTree();
+        var listener1 = 'listener1';
+        var listener2 = 'listener2';
+        tree.addListener(['colors'], listener1);
+        tree.addListener(['colors', 'green'], listener2);
+        expect(tree.children.colors.listeners).eql([listener1]);
+        expect(tree.children.colors.children.green.listeners).eql([listener2]);
+        tree.removeListener(['colors'], listener1);
+        expect(tree.children.colors.listeners).eql(null);
+        expect(tree.children.colors.children.green.listeners).eql([listener2]);
+      });
+    });
+  });
+  describe('forEach', function() {
+    function expectResults(expected, done) {
+      var pending = expected.slice();
+      return function(result) {
+        var value = pending.shift();
+        expect(value).eql(result);
+        if (pending.length > 0) return;
+        done();
+      };
+    }
+    it('can be called without listeners', function(done) {
+      var tree = new EventTree();
+      tree.forEach([], done);
+      done();
+    });
+    it('calls a callback with all direct listeners', function(done) {
+      var tree = new EventTree();
+      var listener1 = 'listener1';
+      var listener2 = 'listener2';
+      tree.addListener([], listener1);
+      tree.addListener([], listener2);
+      var callback = expectResults([listener1, listener2], done);
+      tree.forEach([], callback);
+    });
+    it('removeListener stops listener from being returned', function(done) {
+      var tree = new EventTree();
+      var listener1 = 'listener1';
+      var listener2 = 'listener2';
+      tree.addListener([], listener1);
+      tree.addListener([], listener2);
+      tree.removeListener([], listener1);
+      var callback = expectResults([listener2], done);
+      tree.forEach([], callback);
+    });
+    it('calls a callback with all descendant listeners in depth order', function(done) {
+      var tree = new EventTree();
+      var listener1 = 'listener1';
+      var listener2 = 'listener2';
+      var listener3 = 'listener3';
+      var listener4 = 'listener4';
+      var listener5 = 'listener5';
+      tree.addListener(['colors', 'green'], listener1);
+      tree.addListener(['colors', 'red'], listener2);
+      tree.addListener(['colors', 'red'], listener3);
+      tree.addListener([], listener4);
+      tree.addListener(['colors'], listener5);
+      var callback = expectResults([listener4, listener5, listener1, listener2, listener3], done);
+      tree.forEach([], callback);
+    });
+    it('calls a callback with all parent listeners in depth order', function(done) {
+      var tree = new EventTree();
+      var listener1 = 'listener1';
+      var listener2 = 'listener2';
+      var listener3 = 'listener3';
+      var listener4 = 'listener4';
+      var listener5 = 'listener5';
+      tree.addListener(['colors', 'green'], listener1);
+      tree.addListener(['colors', 'red'], listener2);
+      tree.addListener(['colors', 'red'], listener3);
+      tree.addListener([], listener4);
+      tree.addListener(['colors'], listener5);
+      var callback = expectResults([listener4, listener5, listener1], done);
+      tree.forEach(['colors', 'green'], callback);
+    });
+    it('does not call for peers or peer children', function(done) {
+      var tree = new EventTree();
+      var listener1 = 'listener1';
+      var listener2 = 'listener2';
+      var listener3 = 'listener3';
+      var listener4 = 'listener4';
+      var listener5 = 'listener5';
+      tree.addListener([], listener1);
+      tree.addListener(['colors'], listener2);
+      tree.addListener(['colors', 'green'], listener3);
+      tree.addListener(['textures'], listener4);
+      tree.addListener(['textures', 'smooth'], listener5);
+      var callback = expectResults([listener1, listener4, listener5], done);
+      tree.forEach(['textures'], callback);
+    });
+  });
+});
