f4939ab347d857376a0a0c6b3678105c17f4083e
Nate Smith <nate@lever.co>
2019-07-17 15:46:36 -0700

    make CollectionCounter tests self contained

--- test/Model/CollectionCounter.js
+++ test/Model/CollectionCounter.js
@@ -2,28 +2,35 @@ var expect = require('../util').expect;
 var CollectionCounter = require('../../lib/Model/CollectionCounter');
 
 describe('CollectionCounter', function() {
-  var cc = new CollectionCounter();
-
   it('increment', function() {
-    expect(cc.toJSON()).to.be(undefined);
-    expect(cc.get('numbers', 'one')).to.be(undefined);
-    expect(cc.increment('numbers', 'one')).to.equal(1);    
-    expect(cc.increment('numbers', 'one')).to.equal(2);    
+    var counter = new CollectionCounter();
+    expect(counter.get('colors', 'green')).to.be(undefined);
+    expect(counter.increment('colors', 'green')).to.equal(1);
+    expect(counter.increment('colors', 'green')).to.equal(2);
+    expect(counter.get('colors', 'green')).to.equal(2);
   });
+
   it('toJSON', function() {
-    expect(cc.toJSON()).to.eql({
-      numbers: {
-        one: 2
+    var counter = new CollectionCounter();
+    expect(counter.toJSON()).to.be(undefined);
+    expect(counter.increment('colors', 'green')).to.equal(1);
+    expect(counter.increment('colors', 'green')).to.equal(2);
+    expect(counter.toJSON()).to.eql({
+      colors: {
+        green: 2
       }
     });
   });
+
   it('decrement', function() {
-    expect(cc.get('numbers', 'one')).to.equal(2);    
+    var counter = new CollectionCounter();
+    expect(counter.increment('colors', 'green'));
+    expect(counter.increment('colors', 'green'));
 
-    expect(cc.decrement('numbers', 'one')).to.equal(1);
-    expect(cc.decrement('numbers', 'one')).to.equal(0);    
+    expect(counter.decrement('colors', 'green')).to.equal(1);
+    expect(counter.decrement('colors', 'green')).to.equal(0);
 
-    expect(cc.get('numbers', 'one')).to.be(undefined);
-    expect(cc.toJSON()).to.be(undefined);
+    expect(counter.get('colors', 'green')).to.be(undefined);
+    expect(counter.toJSON()).to.be(undefined);
   });
 });
