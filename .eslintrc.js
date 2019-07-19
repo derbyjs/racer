7c0708ca438699180a72831a7fb2156e665bdf96
Nate Smith <nate@lever.co>
2019-07-19 15:53:29 -0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,37 @@
+// The ESLint ecmaVersion argument is inconsistently used. Some rules will ignore it entirely, so if the rule has
+// been set, it will still error even if it's not applicable to that version number. Since Google sets these
+// rules, we have to turn them off ourselves.
+const DISABLED_ES6_OPTIONS = {
+  'no-var': 'off',
+  'prefer-rest-params': 'off',
+  'prefer-spread': 'off',
+  // Not supported in ES3
+  'comma-dangle': ['error', 'never']
+};
+
+const CUSTOM_RULES = {
+  'one-var': 'off',
+  // We control our own objects and prototypes, so no need for this check
+  'guard-for-in': 'off',
+  // Google prescribes different indents for different cases. Let's just use 2 spaces everywhere. Note that we have
+  // to override ESLint's default of 0 indents for this.
+  'indent': ['error', 2, {'SwitchCase': 1}],
+  // Less aggressive line length than Google, which is especially useful when we have a lot of callbacks in our code
+  'max-len': ['error', {code: 120, tabWidth: 2, ignoreUrls: true}],
+  // Google overrides the default ESLint behaviour here, which is slightly better for catching erroneously unused variables
+  'no-unused-vars': ['error', {vars: 'all', args: 'after-used'}],
+  'require-jsdoc': 'off',
+  'valid-jsdoc': 'off'
+};
+
 module.exports = {
-  extends: 'xo',
-  rules: {
-    'block-scoped-var': 'off',
-    'curly': ['error', 'multi-line', 'consistent'],
-    'eqeqeq': ['error', 'allow-null'],
-    'guard-for-in': 'off',
-    'indent': ['error', 2, {SwitchCase: 1}],
-    'max-len': ['off', 80, 4, {
-      ignoreComments: true,
-      ignoreUrls: true
-    }],
-    'no-eq-null': 'off',
-    'no-implicit-coercion': 'off',
-    'no-nested-ternary': 'off',
-    'no-redeclare': 'off',
-    'no-undef-init': 'off',
-    'no-unused-expressions': ['error', {allowShortCircuit: true}],
-    'one-var': ['error', {initialized: 'never'}],
-    'require-jsdoc': 'off',
-    'space-before-function-paren': ['error', 'never'],
-    'valid-jsdoc': ['off', {
-      requireReturn: false,
-      prefer: {
-        returns: 'return'
-      }
-    }],
-  }
+  extends: 'google',
+  parserOptions: {
+    ecmaVersion: 3
+  },
+  rules: Object.assign(
+    {},
+    DISABLED_ES6_OPTIONS,
+    CUSTOM_RULES
+  ),
 };
