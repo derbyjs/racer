derbyjs/racer

commit a6374765621745d3e76977b8483e4263ba4a61c5

Do not garbage collect a doc if it has pending ops in Share

lib/Model/subscriptions.js
@@ -172,6 +172,7 @@ Model.prototype._maybeUnloadDoc = function(collectionName, id) {
   if (!doc) return;
 
   if (this._hasDocReferences(collectionName, id)) return;
+  if (doc.shareDoc && doc.shareDoc.hasPending()) return;
 
   var previous = doc.get();
 

commit a320772a758495e2c8ed41f396e0c9b8985f1f7d

Fix regression in doc unfetch with pending ops, introduced in a637476

lib/Model/subscriptions.js
@@ -168,6 +168,7 @@ Model.prototype.unsubscribeDoc = function(collectionName, id, cb) {
 // Removes the document from the local model if the model no longer has any
 // remaining fetches or subscribes via a query or direct loading
 Model.prototype._maybeUnloadDoc = function(collectionName, id) {
+  var model = this;
   var doc = this.getDoc(collectionName, id);
   if (!doc) return;
 
@@ -179,16 +180,22 @@ Model.prototype._maybeUnloadDoc = function(collectionName, id) {
   // Thus, if we remove the doc from Racer's model but don't remove it from
   // ShareDB, we can end up with an inconsistent state, with the data existing
   // in ShareDB not reflected in the racer model data
-  if (doc.shareDoc && doc.shareDoc.hasPending()) return;
+  if (doc.shareDoc && doc.shareDoc.hasPending()) {
+    doc.shareDoc.whenNothingPending(unloadDoc);
+  } else {
+    unloadDoc();
+  }
 
-  var previous = doc.get();
+  function unloadDoc() {
+    var previous = doc.get();
 
-  // Remove doc from Racer
-  this.root.collections[collectionName].remove(id);
-  // Remove doc from Share
-  if (doc.shareDoc) doc.shareDoc.destroy();
+    // Remove doc from Racer
+    model.root.collections[collectionName].remove(id);
+    // Remove doc from Share
+    if (doc.shareDoc) doc.shareDoc.destroy();
 
-  this.emit('unload', [collectionName, id], [previous, this._pass]);
+    model.emit('unload', [collectionName, id], [previous, this._pass]);
+  }
 };
 
 Model.prototype._hasDocReferences = function(collectionName, id) {