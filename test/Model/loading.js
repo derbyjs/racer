derbyjs/racer

commit a6374765621745d3e76977b8483e4263ba4a61c5

Do not garbage collect a doc if it has pending ops in Share

lib/Model/subscriptions.js
@@ -172,6 +172,7 @@ Model.prototype._maybeUnloadDoc = function(collectionName, id) {
   if (!doc) return;
 
   if (this._hasDocReferences(collectionName, id)) return;
+  if (doc.shareDoc && doc.shareDoc.hasPending()) return;
 
   var previous = doc.get();
 

commit 5663570b0872cafb401658be9cda6856ce2f9a62

Add test for doc unfetch regression introduced by a637476

test/Model/loading.js
@@ -2,19 +2,19 @@ var expect = require('../util').expect;
 var racer = require('../../lib/index');
 
 describe('loading', function() {
-  describe('subscribe', function() {
-    beforeEach(function(done) {
-      this.backend = racer.createBackend();
-      // Add a delay on all messages to help catch race issues
-      var delay = 5;
-      this.backend.use('receive', function(request, next) {
-        delay++;
-        setTimeout(next, delay);
-      });
-      this.model = this.backend.createModel();
-      this.model.connection.on('connected', done);
+  beforeEach(function(done) {
+    this.backend = racer.createBackend();
+    // Add a delay on all messages to help catch race issues
+    var delay = 5;
+    this.backend.use('receive', function(request, next) {
+      delay++;
+      setTimeout(next, delay);
     });
+    this.model = this.backend.createModel();
+    this.model.connection.on('connected', done);
+  });
 
+  describe('subscribe', function() {
     it('calls back simultaneous subscribes to the same document', function(done) {
       var doc = this.model.connection.get('colors', 'green');
       expect(doc.version).equal(null);
@@ -42,6 +42,32 @@ describe('loading', function() {
         if (err) return done(err);
         expect(doc.subscribed).equal(true);
         model.subscribe('colors.green', done);
+      });
+    });
+  });
+
+  describe('unfetch', function() {
+    it('unloads doc after Share doc has nothing pending', function() {
+      var setupModel = this.backend.createModel();
+      var model = this.model;
+      setupModel.add('colors', {id: 'green', hex: '00ff00'}, function(err) {
+        if (err) return done(err);
+
+        model.fetch('colors.green', function(err) {
+          if (err) return done(err);
+          expect(model.get('colors.green.hex')).to.equal('00ff00');
+          // Queue up a pending op.
+          model.set('colors.green.hex', '00ee00');
+          // Unfetch. The pending op prevents the doc from immedialy being unloaded.
+          model.unfetch('colors.green');
+          // Once there's nothing pending on the model/doc...
+          model.whenNothingPending(function() {
+            // Racer doc should be unloaded.
+            expect(model.get('colors.green')).to.equal(undefined);
+            // Share doc should be unloaded too.
+            expect(model.connection.getExisting('colors', 'green')).to.equal(undefined);
+          });
+        });
       });
     });
   });