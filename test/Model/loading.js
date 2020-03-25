c14610a2ff68a753272a8777a072c9dd15d4acdf
Eric Hwang <eric.hwang@lever.co>
2020-03-25 13:02:59 -0700

    Fix deferred unload (see a320772a) when unfetching then synchronously subscribing
    When a doc gets unfetched, #276 adds a Share Doc whenNothingPending listener to unload the doc later, instead of doing nothing, which was the cause of a memory leak introduced in #266.
    
    However, Racer keeps reference counts itself for fetches and subscriptions, so an immediate subscribe on the same Racer doc does not affect the Share doc at all, which means the doc would still get unloaded, erroneously.
    
    This fix instead retries the Racer _maybeUnloadDoc in that scenario, instead of doing an immediate unload. That guards against Racer getting new doc references while the Share Doc whenNotingPending is in progress.

--- test/Model/loading.js
+++ test/Model/loading.js
@@ -46,28 +46,56 @@ describe('loading', function() {
     });
   });
 
-  describe('unfetch', function() {
-    it('unloads doc after Share doc has nothing pending', function(done) {
-      var setupModel = this.backend.createModel();
-      var model = this.model;
-      setupModel.add('colors', {id: 'green', hex: '00ff00'}, function(err) {
-        if (err) return done(err);
+  describe('unfetch deferred unload', function() {
+    beforeEach(function(done) {
+      this.setupModel = this.backend.createModel();
+      this.setupModel.add('colors', {id: 'green', hex: '00ff00'}, done);
+    });
 
-        model.fetch('colors.green', function(err) {
-          if (err) return done(err);
-          expect(model.get('colors.green.hex')).to.equal('00ff00');
-          // Queue up a pending op.
-          model.set('colors.green.hex', '00ee00');
-          // Unfetch. The pending op prevents the doc from immedialy being unloaded.
-          model.unfetch('colors.green');
-          // Once there's nothing pending on the model/doc...
-          model.whenNothingPending(function() {
-            // Racer doc should be unloaded.
-            expect(model.get('colors.green')).to.equal(undefined);
-            // Share doc should be unloaded too.
-            expect(model.connection.getExisting('colors', 'green')).to.equal(undefined);
-            done();
-          });
+    it('unloads doc after Share doc has nothing pending', function(done) {
+      var model = this.model;
+      model.fetch('colors.green', function(err) {
+        if (err) return done(err);
+        expect(model.get('colors.green.hex')).to.equal('00ff00');
+        // Queue up a pending op.
+        model.set('colors.green.hex', '00ee00');
+        // Unfetch. This triggers the delayed _maybeUnloadDoc.
+        // The pending op causes the doc unload to be delayed.
+        model.unfetch('colors.green');
+        // Once there's nothing pending on the model/doc...
+        model.whenNothingPending(function() {
+          // Racer doc should be unloaded.
+          expect(model.get('colors.green')).to.equal(undefined);
+          // Share doc should be unloaded too.
+          expect(model.connection.getExisting('colors', 'green')).to.equal(undefined);
+          done();
+        });
+      });
+    });
+
+    it('does not unload doc if a subscribe is issued in the meantime', function(done) {
+      var model = this.model;
+      // Racer keeps its own reference counts of doc fetches/subscribes - see `_hasDocReferences`.
+      model.fetch('colors.green', function(err) {
+        if (err) return done(err);
+        expect(model.get('colors.green.hex')).to.equal('00ff00');
+        // Queue up a pending op.
+        model.set('colors.green.hex', '00ee00');
+        // Unfetch. This triggers the delayed _maybeUnloadDoc.
+        // The pending op causes the doc unload to be delayed.
+        model.unfetch('colors.green');
+        // Immediately subscribe to the same doc.
+        // This causes the doc to be kept in memory, even after the unfetch completes.
+        model.subscribe('colors.green');
+        // Once there's nothing pending on the model/doc...
+        model.whenNothingPending(function() {
+          // Racer doc should still be present due to the subscription.
+          expect(model.get('colors.green')).to.eql({id: 'green', hex: '00ee00'});
+          // Share doc should be present too.
+          var shareDoc = model.connection.getExisting('colors', 'green');
+          expect(shareDoc).to.have.property('data');
+          expect(shareDoc.data).to.eql({id: 'green', hex: '00ee00'});
+          done();
         });
       });
     });
