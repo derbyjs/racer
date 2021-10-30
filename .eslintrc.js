aafa799c67c9c7182d91e2517908ce6368b564ed
Eric Hwang <eric.hwang@lever.co>
2021-10-29 19:09:16 -0700

    Upgrade devDependencies: eslint 5->8, mocha 6->9, nyc 14->15

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // The ESLint ecmaVersion argument is inconsistently used. Some rules will ignore it entirely, so if the rule has
 // been set, it will still error even if it's not applicable to that version number. Since Google sets these
 // rules, we have to turn them off ourselves.
-const DISABLED_ES6_OPTIONS = {
+var DISABLED_ES6_OPTIONS = {
   'no-var': 'off',
   'prefer-rest-params': 'off',
   'prefer-spread': 'off',
@@ -9,7 +9,7 @@ const DISABLED_ES6_OPTIONS = {
   'comma-dangle': ['error', 'never']
 };
 
-const CUSTOM_RULES = {
+var CUSTOM_RULES = {
   'one-var': 'off',
   // We control our own objects and prototypes, so no need for this check
   'guard-for-in': 'off',
@@ -18,7 +18,7 @@ const CUSTOM_RULES = {
   'indent': ['error', 2, {'SwitchCase': 1}],
   // Less aggressive line length than Google, which is especially useful when we have a lot of callbacks in our code
   'max-len': ['error', {code: 120, tabWidth: 2, ignoreUrls: true}],
-  // Google overrides the default ESLint behaviour here, which is slightly better for catching erroneously unused variables
+  // Go back to default ESLint behaviour here, which is slightly better for catching erroneously unused variables
   'no-unused-vars': ['error', {vars: 'all', args: 'after-used'}],
   'require-jsdoc': 'off',
   'valid-jsdoc': 'off'
@@ -27,11 +27,11 @@ const CUSTOM_RULES = {
 module.exports = {
   extends: 'google',
   parserOptions: {
-    ecmaVersion: 3
+    ecmaVersion: 5
   },
   rules: Object.assign(
     {},
     DISABLED_ES6_OPTIONS,
     CUSTOM_RULES
-  ),
+  )
 };
