1eabf5632d67219817167ac00b0fbbd2b019a2dc
Craig Beck <luckybonza@gmail.com>
2023-06-27 11:19:25 -0700

    Convert Racer to class; resolve circular require w Racer.server

--- src/Racer.ts
+++ src/Racer.ts
@@ -2,29 +2,28 @@ var EventEmitter = require('events').EventEmitter;
 var Model = require('./Model');
 var util = require('./util');
 
-module.exports = Racer;
 
-function Racer() {
-  EventEmitter.call(this);
+class Racer extends EventEmitter {
+  Model: typeof Model = Model;
+  util = util;
+  use = util.use;
+  serverUse = util.serverUse;
+
+  constructor() {
+    super();
+  }
+
+  createModel(data) {
+    var model = new Model();
+    if (data) {
+      model.createConnection(data);
+      model.unbundle(data);
+    }
+    return model;
+  }
 }
 
-util.mergeInto(Racer.prototype, EventEmitter.prototype);
-
-// Make classes accessible for use by plugins and tests
-Racer.prototype.Model = Model;
-Racer.prototype.util = util;
-
-// Support plugins on racer instances
-Racer.prototype.use = util.use;
-Racer.prototype.serverUse = util.serverUse;
-
-Racer.prototype.createModel = function(data) {
-  var model = new Model();
-  if (data) {
-    model.createConnection(data);
-    model.unbundle(data);
-  }
-  return model;
-};
+// exports before serverRequire as Racer.server has circular require
+module.exports = Racer;
 
 util.serverRequire(module, './Racer.server');
