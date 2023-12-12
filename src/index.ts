da0beb4d365ca4b3d8fb62e38bca689241f18a5f
Craig Beck <luckybonza@gmail.com>
2023-12-11 16:41:55 -0800

    Testing with exported anmespace and types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 import { Racer } from './Racer';
-module.exports = new Racer();
+import { Model } from './Model';
+import * as util from './util';
+import { RacerBackend } from './Backend';
+import { Query } from './Model/Query';
+
+// module.exports = new Racer();
+const { use, serverUse } = util;
+
+export { Model };
+export { Query };
+export { Racer };
+export { RacerBackend };
+export { use, serverUse };
+export { util };
+export const racer = new Racer();
+
+export function createModel(data) {
+  var model = new Model();
+  if (data) {
+    model.createConnection(data);
+    model.unbundle(data);
+  }
+  return model;
+}
+
+export function createBackend(options) {
+  return new RacerBackend(racer, options);
+};
